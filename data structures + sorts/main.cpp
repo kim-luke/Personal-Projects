2e2ef2b3acb9e68eb3203411d7ddb3fd32c64097
kim-luke <48031366+kim-luke@users.noreply.github.com>
2020-07-30 19:22:05 -0500

    Update main.cpp

--- data structures + sorts/main.cpp
+++ data structures + sorts/main.cpp
@@ -1,13 +1,21 @@
+/*
+	Luke Kim
+
+	practicing: bubble sort, insertion sort, selection sort, quick sort, merge sort
+
+	also: will be tracking their time complexities and comparing them to each other
+
+*/
+
 #include <iostream>
 #include <string>
 #include <stdlib.h>
-#include <ctime>
-#include <stack>
+#include <time.h>
 using namespace std;
 
 void fillArray(int array[], int size) {
 	for (int i = 0; i < size; i++) {
-		array[i] = rand() % 20 + 1;
+		array[i] = rand() % 200 + 1;
 	}
 }
 
@@ -17,12 +25,27 @@ void deleteArray(int array[]) {
 
 void showArray(int array[], int size) {
 
-	cout << "Array contents: " << endl;
+	cout << "\nArray contents: " << endl;
 
 	for (int i = 0; i < size; i++) {
-		cout << "Index " << i << ": " << array[i] << endl;
+		cout << array[i] << " ";
 	}
 	cout << endl;
+}
+
+// bubble sort
+void bubbleSort(int array[], int size) {
+	int temp = 0;
+	for (int i = 0; i < size - 1; i++) {
+		
+		for (int j = 0; j < size - i - 1; j++) {
+			if (array[j] > array[j + 1]) {
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+			}
+		}		
+	}
 }
 
 int main() {
@@ -30,14 +53,25 @@ int main() {
 	// generates a true random seed
 	srand(time(NULL));
 
-	// initialize array
+	double bubbleTime, insertionTime, selectionTime, quickTime, mergeTime;
+	clock_t start, end;
+
+	// initialize the array
 	int size;
 	cout << "Insert size for your array" << endl;
 	cin >> size;
 	int* array = new int[size];
 
 	fillArray(array, size);
-	showArray(array, size);
+	//showArray(array, size);
+
+	start = clock();
+	bubbleSort(array, size);
+	end = clock();
+	
+	bubbleTime = (double)(end - start) / CLOCKS_PER_SEC;
+	cout << "\nTIME ELAPSED FOR BUBBLE SORT OF SIZE " << size << ": " << bubbleTime << " seconds" << endl;
+	//showArray(array, size);
 
 	deleteArray(array);
 	system("pause");
